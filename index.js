12eecef8e60b4cf91815e0abd60d875f5b4437ef
lixelamos <amosbett99@gmail.com>
2023-04-02 01:41:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,60 @@
-/* Your Code Here */
-
+function createEmployeeRecord(employeeArray) {
+    return {
+      firstName: employeeArray[0],
+      familyName: employeeArray[1],
+      title: employeeArray[2],
+      payPerHour: employeeArray[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  function createEmployeeRecords(arrays) {
+    return arrays.map(createEmployeeRecord);
+  }
+  
+  function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(' ')
+    this.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour),
+      date: date
+    })
+    return this
+  }
+  
+  
+  function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(" ")
+  
+    this.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour),
+      date: date
+    })
+  
+    return this
+  }
+  
+  function hoursWorkedOnDate(date) {
+    const timeInEvent = this.timeInEvents.find(event => event.date === date)
+    const timeOutEvent = this.timeOutEvents.find(event => event.date === date)
+  
+    if (!timeInEvent || !timeOutEvent) {
+      return 0
+    }
+  
+    const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100
+  
+    return hoursWorked
+  }
+  
+  
+  function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+  }
+  
+  
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,4 +75,13 @@ const allWagesFor = function () {
 
     return payable
 }
-
+function findEmployeeByFirstName(srcArray, firstName) {
+  return srcArray.find(employee => employee.firstName === firstName)
+}
+function calculatePayroll(employeeRecords) {
+  let totalPay = 0;
+  employeeRecords.forEach(function (record) {
+    totalPay += allWagesFor.call(record);
+  });
+  return totalPay;
+}
